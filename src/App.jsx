ed630151f874ed924209a9ab51f80f0bef2900ce
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-06-20 10:56:53 -0300

    unidade 1 finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './paginas/Inicio/Inicio';
 import SobreMim from './paginas/SobreMim/SobreMim';
 
-const endereco = window.location.pathname;
-
-const pagina = endereco === '/' ? <Inicio /> : <SobreMim />;
-
 function App() {
-  return pagina;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
